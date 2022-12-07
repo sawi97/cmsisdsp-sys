fd9ff66b42e68dd73d9fc17f2be9610ad25c3dbe
sander <sander.wittwer@dengineering.no>
2022-12-07 09:48:24 +0100

    ffi: use libm

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,40 +1,39 @@
 //! VFP FFI Instructions
 //!
 //! TODO: More optimized methods
-use micromath::F32Ext;
 
 // TODO TODO TODO
 #[no_mangle]
 pub unsafe extern "C" fn expf(v: f32) -> f32 {
-    v.exp()
+    libm::expf(v)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn exp(v: f64) -> f64 {
-    (v as f32).exp() as f64
+    libm::exp(v)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn logf(v: f32) -> f32 {
-    v.ln()
+    libm::logf(v)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn log(v: f64) -> f64 {
-    (v as f32).ln() as f64
+    libm::log(v)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn powf(b: f32, e: f32) -> f32 {
-    b.powf(e)
+    libm::powf(b, e)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn sqrtf(v: f32) -> f32 {
-    v.sqrt()  // TODO: VSQRT
+    libm::sqrtf(v)  // TODO: VSQRT
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn sqrt(v: f64) -> f64 {
-    (v as f32).sqrt() as f64  // TODO: VSQRT
+    libm::sqrt(v)  // TODO: VSQRT
 }
