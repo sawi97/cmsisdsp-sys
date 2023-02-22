d6e4dd2199e11c8f81bb5974b835a17ebcae5c56
sander <sander.wittwer@dengineering.no>
2023-02-22 10:33:23 +0100

    cleanup: minor cleanup in build.rs, bump bindgen to 0.64

--- build.rs
+++ build.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::path::{Path, PathBuf};
 
 static CONFIG_KEY: &str = "CMSISDSP_CFG";
+static CMSIS_5_DIR: &str = "cmsis/CMSIS_5";
+static CMSIS_DSP_DIR: &str = "cmsis/CMSIS-DSP";
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
@@ -12,8 +14,8 @@ fn main() {
 
     let outdir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    let dsp_dir = manifest_dir.join("cmsis/CMSIS-DSP");
-    let cmsis5_dir = manifest_dir.join("cmsis/CMSIS_5");
+    let dsp_dir = manifest_dir.join(CMSIS_DSP_DIR);
+    let cmsis5_dir = manifest_dir.join(CMSIS_5_DIR);
 
     let manifest_dir = Path::new(&manifest);
 
@@ -40,64 +42,7 @@ fn main() {
     let bindings = bb.generate().expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let mut rust_source = bindings.to_string();
-
-    // Munge Doxygen comments into something Rustdoc can handle
-    rust_source = rust_source.replace("#[doc = \" @{\"]", "");
-
-    // Format @param as list element
-    let re = regex::Regex::new(r"\s*@[pP]aram\s*(\[(?P<typ>[\w,\s]+)\s*\])?\s*(\\t)?(?P<var>[\w\.]+)\s+").unwrap();
-    rust_source = re.replace_all(&rust_source, " * `$var` $typ - ").into();
-
-    // Format @p/@a/@c arguments as inline code
-    let re = regex::Regex::new(r"@[pac]\s+(?P<var>[\*A-Za-z0-9_\(\)]+)").unwrap();
-    rust_source = re.replace_all(&rust_source, " `$var` ").into();
-
-    // Format NRF_* as ref
-    let re = regex::Regex::new(r"(?P<pre>@(returns?|retval|note)\s+.*)(?P<var>NRF_\w+)").unwrap();
-    rust_source = re.replace_all(&rust_source, "$pre[$var]").into();
-
-    // #nrf_*
-    let re = regex::Regex::new("(?P<pre>#\\[doc.*\\s+)#(?P<var>(nrf|NRF)_\\w+)(?P<post>\\s+.*\"\\])").unwrap();
-    rust_source = re
-        .replace_all(&rust_source, "$pre[$var]$post")
-        .into();
-
-    // Remove @addtogroup stuff
-    let re = regex::RegexBuilder::new(r"^#\[doc.*@addtogroup(.|\n)*?^$")
-        .multi_line(true)
-        .build()
-        .unwrap();
-    rust_source = re.replace_all(&rust_source, "").into();
-
-    // Format @ref as markdown ref
-    let re = regex::Regex::new(r"\s*@(ref|refitem)\s+(?P<var>\w+)(\(\))?").unwrap();
-    rust_source = re.replace_all(&rust_source, " [$var]").into();
-
-    // Format deprecation notice (@deprecated) as deprecated
-    let re = regex::Regex::new("#\\[doc.*@deprecated\\s*(?P<note>.*)\\.*\".*]").unwrap();
-    rust_source = re
-        .replace_all(&rust_source, "#[deprecated(note=\"$note\")]")
-        .into();
-
-    // Format inline @brief
-    let re = regex::Regex::new("#\\[doc = \"\\s*@brief\\s*(?P<msg>.*)\"]").unwrap();
-    rust_source = re
-        .replace_all(&rust_source, "#[doc = \"$msg\"]")
-        .into();
-
-    // Format inline @note as bold
-    let re = regex::Regex::new(r"\s*@note:?\s*").unwrap();
-    rust_source = re.replace_all(&rust_source, "**Note:** ").into();
-
-    // Format @details as a section
-    let re = regex::Regex::new(r"\s*@details?\s*(?P<var>.*)").unwrap();
-    rust_source = re.replace_all(&rust_source, "# Details \n$var").into();
-
-    // // Format some sections as headers
-    rust_source = rust_source.replace("@return ", "# Returns\n");
-    rust_source = rust_source.replace("@returns ", "# Returns\n");
-    rust_source = rust_source.replace("@retval ", "# Returns\n");
+    let mut rust_source = format_docs(bindings.to_string());
 
     // Write bindings to bindings.rs
     std::fs::write(outdir.join("bindings.rs"), rust_source).expect("Couldn't write bindgen output");
@@ -143,4 +88,65 @@ fn parse_cfg(cfg_str: &str, cfg: &mut cmake::Config) {
         println!("adding cmake flag {}={}", &m[1], &m[2]);
         cfg.define(&m[1], &m[2]);
     }
+}
+
+/// Format the Doxygen comments to rustdoc
+fn format_docs(mut source: String) -> String {
+    source = source.replace("#[doc = \" @{\"]", "");
+
+    // Format @param as list element
+    let re = regex::Regex::new(r"\s*@[pP]aram\s*(\[(?P<typ>[\w,\s]+)\s*\])?\s*(\\t)?(?P<var>[\w\.]+)\s+").unwrap();
+    source = re.replace_all(&source, " * `$var` $typ - ").into();
+
+    // Format @p/@a/@c arguments as inline code
+    let re = regex::Regex::new(r"@[pac]\s+(?P<var>[\*A-Za-z0-9_\(\)]+)").unwrap();
+    source = re.replace_all(&source, " `$var` ").into();
+
+    // Format NRF_* as ref
+    let re = regex::Regex::new(r"(?P<pre>@(returns?|retval|note)\s+.*)(?P<var>NRF_\w+)").unwrap();
+    source = re.replace_all(&source, "$pre[$var]").into();
+
+    // #nrf_*
+    let re = regex::Regex::new("(?P<pre>#\\[doc.*\\s+)#(?P<var>(nrf|NRF)_\\w+)(?P<post>\\s+.*\"\\])").unwrap();
+    source = re
+        .replace_all(&source, "$pre[$var]$post")
+        .into();
+
+    // Remove @addtogroup stuff
+    let re = regex::RegexBuilder::new(r"^#\[doc.*@addtogroup(.|\n)*?^$")
+        .multi_line(true)
+        .build()
+        .unwrap();
+    source = re.replace_all(&source, "").into();
+
+    // Format @ref as markdown ref
+    let re = regex::Regex::new(r"\s*@(ref|refitem)\s+(?P<var>\w+)(\(\))?").unwrap();
+    source = re.replace_all(&source, " [$var]").into();
+
+    // Format deprecation notice (@deprecated) as deprecated
+    let re = regex::Regex::new("#\\[doc.*@deprecated\\s*(?P<note>.*)\\.*\".*]").unwrap();
+    source = re
+        .replace_all(&source, "#[deprecated(note=\"$note\")]")
+        .into();
+
+    // Format inline @brief
+    let re = regex::Regex::new("#\\[doc = \"\\s*@brief\\s*(?P<msg>.*)\"]").unwrap();
+    source = re
+        .replace_all(&source, "#[doc = \"$msg\"]")
+        .into();
+
+    // Format inline @note as bold
+    let re = regex::Regex::new(r"\s*@note:?\s*").unwrap();
+    source = re.replace_all(&source, "**Note:** ").into();
+
+    // Format @details as a section
+    let re = regex::Regex::new(r"\s*@details?\s*(?P<var>.*)").unwrap();
+    source = re.replace_all(&source, "# Details \n$var").into();
+
+    // // Format some sections as headers
+    source = source.replace("@return ", "# Returns\n");
+    source = source.replace("@returns ", "# Returns\n");
+    source = source.replace("@retval ", "# Returns\n");
+
+    source
 }
