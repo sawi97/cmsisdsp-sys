228f923963f04b2185dc7896b3b89dffd5d8d1be
sander <sander.wittwer@dengineering.no>
2023-05-04 11:17:22 +0200

    lib: remove std feature check as it is no longer used

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! System bindings for CMSIS DSP
-#![cfg_attr(not(feature = "std"), no_std)]
+#![no_std]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
